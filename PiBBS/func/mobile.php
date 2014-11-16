68ba8431252899505b1fe5570a38ceead2507fad
Tom Chen <txchen@gmail.com>
2014-11-16 12:06:42 -1000

    Update mobile.php
    Check to make sure $_SERVER["HTTP_USER_AGENT"] is set. Avoid error_log msg.

--- PiBBS/func/mobile.php
+++ PiBBS/func/mobile.php
@@ -4,6 +4,7 @@
 // From: http://stackoverflow.com/questions/6524301/detect-mobile-browser
 //
 function isMobile() {
+    if (! isset($_SERVER["HTTP_USER_AGENT"]) ) return false;
     return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 }
 ?>
