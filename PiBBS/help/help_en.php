3d6dd0d9e074887fdde66eaa9f233fdaa6c80589
Tom Chen <txchen@gmail.com>
2014-11-16 17:44:36 -1000

    Update help_en.php

--- PiBBS/help/help_en.php
+++ PiBBS/help/help_en.php
@@ -10,7 +10,11 @@
 <br/>@[img src="{image link}" width="400" height="200"]
 <li>To show a video (Youtube, Youku etc.):
 <br/>E.g., the original link is &lt;iframe width="560" height="315" src=".." .. allowfullscreen&gt;&lt;/iframe&gt;
-<br/>Change to: @[iframe width="560" height="315" src=".." .. allowfullscreen]
+<br/>Change to: @[vframe width="560" height="315" src=".." .. allowfullscreen]
+<li>To display an embedded webpage:
+<br/>E.g., the webpage address is: url
+<br/>Change to: @[iframe width="100%" height="1000" src="url" style="border: 0px;"]
+<br/>Note: For videos, use @[iframe] instead of @[vframe] may cause inconsistent formatting on mobile devices.
 <li>To show a segment of code:
 <br/>@[code] {your code} @[/code]
 <li>To show a segment of code is a textarea with adjustable height:
